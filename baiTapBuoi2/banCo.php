88d6222bebb40d7d10c341c771ce1f81effa3c6d
Thuan Pham <phamtanthuan2311@gmail.com>
2022-09-12 16:32:21 +0700

    [👌Auto commit] I am even stupider than I thought

---
+++ baiTapBuoi2/banCo.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title></title>
+    <style>
+      td {
+        width: 25px;
+        height: 25px;
+      }
+    </style>
+  </head>
+  <body>
+<table border="1">
+    <?php
+    $row = 4;
+    $column = 6;
+    for ($i = 0 ; $i < $row ; $i++) {
+    echo "<tr>";
+        for ($j = 0; $j < $column ; $j++) {
+            $tong = $i + $j;
+            if ($tong % 2 == 0 ) {
+                echo "<td >&nbsp;</td>";
+            } else {
+                echo "<td style=\"background-color=\'black\';color=\'white\'\">&nbsp;</td>";
+            }
+        }
+    echo "</tr>";
+}
+    ?>
+</table>
+  </body>
+</html>
